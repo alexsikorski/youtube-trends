77e766d3e020e8db174ffb74514a1943ff32b332
as16815 <as16815@essex.ac.uk>
2020-05-26 20:35:08 +0100

    Analyser now displays outputs the following; video title, topics, tags and views.

--- analyser.py
+++ analyser.py
@@ -1,15 +1,36 @@
 import pickle
 
 
-def load_videos(name ):
+def load_pkl(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
+
 def main():
-    videos = load_videos("vid_master")
-    print(videos)
+    videos = load_pkl("vid_master")
+    topic_definitions = load_pkl("topic_definitions")
+
     for vid in videos:
-        print(vid.get("topics"))
+        print("Title: " + vid.get("title"))
+
+        topics = vid.get("topics")
+
+        # defines topic definition
+        if topics is not None:
+            for i, topic in enumerate(topics):
+                for (key, value) in topic_definitions.items():
+                    if topic == key:
+                        topics[i] = value
+
+        # removes duplicates
+        if topics is not None:
+            topics_no_dupes = list(dict.fromkeys(topics))
+
+        print("Topics: " + str(topics_no_dupes))
+        print("Tags: " + str(vid.get("tags")))
+        print("Views: " + vid.get("views"))
+        print("")
+
 
 if __name__ == "__main__":
     main()
